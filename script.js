cd7ca2e6af575ea77dd362be60345222c34f21f2
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-07-27 12:20:09 +0530

    fetching the data using api

---
+++ script.js
@@ -0,0 +1,27 @@
+const url = "https://cricbuzz-cricket.p.rapidapi.com/mcenter/v1/40381/scard"
+const options = {
+    method: 'GET',
+    headers: {
+        'x-rapidapi-key': 'a2038168camshbdb916781fbbddap1efab6jsndbe2a509b615',
+        'x-rapidapi-host': 'cricbuzz-cricket.p.rapidapi.com'
+    }
+};
+const getinfo = async () => {
+    try {
+        const First_Team = document.querySelector('.teamname1')
+        const Second_Team = document.querySelector(".teamname2");
+
+        // Fetching the  Response 
+        const response = await fetch(url, options);
+        const res = await response.json();
+        const batteam = await res.scoreCard[0].batTeamDetails.batTeamShortName
+        const bowlteam = await res.scoreCard[0].bowlTeamDetails.bowlTeamShortName
+        console.log({ batteam, bowlteam })
+
+       
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+getinfo
